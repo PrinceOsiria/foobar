8e43bc19ec9247ec993ff459bf7f18f84ea42112
PrinceOsiria <37147038+PrinceOsiria@users.noreply.github.com>
2020-11-15 18:29:23 -0500

    Update main.py

--- Lovely-Lucky-Lambs/main.py
+++ Lovely-Lucky-Lambs/main.py
@@ -1,57 +1,38 @@
-
-#####Functions
-# input: number of lambs
-# output: an integer which represents the difference between the minimum and maximum number of henchmen who can share the LAMBs
+#####Function
 def solution(lambs):
-	## Variables
-	minimum = 1 # The most junior henchman (with the least seniority) gets exactly 1 LAMB.
+    ## Variables
+    minimum = 1
+    stingy = [minimum]
+    generous = [minimum]
 
+    ## Generous Method - More lambs per employee, less employees included
+    number = minimum
+    while sum(generous) + number <= lambs: 
+        if (number!=minimum) and number>=sum(generous[-2:number]) and number==sum(generous[-1:number])*2:
+            generous.append(number)
+        number += 1
 
-	## Number Validator
-	def number_is_valid(number, method, style): # Using the provided rules, each input is checked with it's corresponding style
-		if style == "stingy":
-			if number>=sum(method[-2:number]) and number<=sum(method[-1:number]*2): # Formula for maximum employees per set of lambs
-				return True
-		elif style == "generous":
-			if (number!=minimum) and number>sum(method[-2:number]) and number==sum(method[-1:number]*2): # Formula for maximum lambs per employee
-				return True
+    ## Stingy Method - Less lambs per employee, more employees included
+    number = minimum
+    while sum(stingy) + number <= lambs: 
+        if number>=sum(stingy[-2:number]) and number<=sum(stingy[-1:number])*2:
+            stingy.append(number)
 
-
-	## Generous Method
-	generous = [minimum] # (There will always be at least 1 henchman on a team.)
-	number = minimum
-	while sum(generous) + number <= lambs: 
-		if number_is_valid(number, generous, "generous"):
-			generous.append(number)
-		number += 1
-
-
-	##Stingy Method
-	stingy = [minimum] # (There will always be at least 1 henchman on a team.)
-	number = minimum
-	while sum(stingy) + number <= lambs: 
-		if number_is_valid(number, stingy, "stingy"):
-			stingy.append(number)
-		number += 1
-
-
-	## Return
-	if debug == True:
-		return "\nInput: "+str(lambs)+"\n"+"Generous:"+str(generous),"Stingy: "+str(stingy), "Output: "+str(abs(len(generous) - len(stingy))) # Returns the difference between the number of henchmen receiving lambs via the provided methods
-	else:
-		return abs(len(stingy) - len(generous))
-
-
-#####Debugging
+## Return
+    if debug:
+        return f"\nInput: {str(lambs)}\nGenerous:{str(generous)}\nStingy: {str(stingy)}\nOutput: {abs( len(generous) - len(stingy) )}" # Returns the input as well as the contents of the generous and stingy lists
+    else:
+        return abs(len(stingy) - len(generous)) # Returns the difference between the number of henchmen receiving lambs via the provided methods
+#####Main
 var = 13
 
-debug = True
-top = 13
-bot = 10
+debug = 1
+start= 10
+end= 13
 
-if debug == True:
-	for test in list(range(bot,top+1)):
-		for output in solution(test):
-			print(output)
+if debug:
+    for test in range(start,end+1):
+        for output in solution(test):
+            print(output)
 else:
-	print(solution(var))
+    print(solution(var))
